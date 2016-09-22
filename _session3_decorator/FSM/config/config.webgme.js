9c3a9e06b036fd4f7773dae79591e0541523e9e7
Patrik Meijer <patrik85@isis.vanderbilt.edu>
2016-09-22 18:49:12 -0500

    Generated code from command 'webgme new viz GoogleMap'

--- _session3_decorator/FSM/config/config.webgme.js
+++ _session3_decorator/FSM/config/config.webgme.js
@@ -6,20 +6,25 @@
 var config = require('webgme/config/config.default'),
     validateConfig = require('webgme/config/validator');
 
-
 // The paths can be loaded from the webgme-setup.json
-config.seedProjects.basePaths.push('src/seeds/FSM');
-config.plugin.basePaths.push('src/plugins');
-config.visualization.decoratorPaths.push('src/decorators');
+config.plugin.basePaths.push(__dirname + '/../src/plugins');
+config.visualization.decoratorPaths.push(__dirname + '/../src/decorators');
+config.seedProjects.basePaths.push(__dirname + '/../src/seeds/FSM');
 
+
+
+config.visualization.panelPaths.push(__dirname + '/../src/visualizers/panels');
 
 
 
 
 // Visualizer descriptors
-
+config.visualization.visualizerDescriptors.push(__dirname + '/../src/visualizers/Visualizers.json');
 // Add requirejs paths
-
+config.requirejsPaths = {
+  'panels': './src/visualizers/panels',
+  'widgets': './src/visualizers/widgets'
+};
 
 
 config.mongo.uri = 'mongodb://127.0.0.1:27017/fsm';
